c7850d5109d59e4d3b8aa0158f32a32002135189
unknown <mfaqihuddin23@gmail.com>
2022-04-21 09:53:41 +0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./assets/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors:{
+        pop:{
+          50: '#6F6F6F',
+          100: '#394149',
+          200: '#FDA403',
+          300: '#BEBEBE',
+        }
+      }
+    },
   },
   plugins: [],
 }
